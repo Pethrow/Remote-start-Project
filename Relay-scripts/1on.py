8d63297473fe210a0b31ef7603cef785f4c96ed9
Pethrow <82409552+Pethrow@users.noreply.github.com>
2022-01-13 20:14:41 -0500

    dix

--- Relay-scripts/1on.py
+++ Relay-scripts/1on.py
@@ -4,7 +4,7 @@ import time
 #Set mode to BCM to keep numbers printed on the board
 GP.setmode(GP.BCM)
 #setup pins
-GPIO.setup(21,GP.OUT)
+GP.setup(21,GP.OUT)
 
 try:
  while True:
--- Relay-scripts/1on.py.bak
+++ Relay-scripts/1on.py.bak
@@ -4,7 +4,7 @@ import time
 #Set mode to BCM to keep numbers printed on the board
 GP.setmode(GP.BCM)
 #setup pins
-GP.setup(21,OUT)
+GPIO.setup(21,GP.OUT)
 
 try:
  while True:
