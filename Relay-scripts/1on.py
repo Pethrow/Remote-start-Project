ab55945ac443a3cd7700f4394485fd5e36bc0779
Jim bob <tacomaxco@gmail.com>
2022-01-13 20:41:27 -0500

    26

--- Relay-scripts/1on.py
+++ Relay-scripts/1on.py
@@ -4,11 +4,12 @@ import time
 #Set mode to BCM to keep numbers printed on the board
 GP.setmode(GP.BCM)
 #setup pins
-GP.setup(21,GP.OUT)
+GP.setup(26,GP.OUT)
 
 try:
  while True:
-  GP.output(21,True)
-
+  GP.output(26,True)
+  time.sleep(5);
+  GP.outpuy(26,False)
 finally:
  GP.cleanup()
